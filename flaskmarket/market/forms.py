e45c72ac41c189a1b2ddd2ef9ff2d1639a05172d
atomicabdul4life <abdullateefabdulkareem60@gmail.com>
2023-02-12 12:37:11 +0000

    register page updated

--- flaskmarket/market/forms.py
+++ flaskmarket/market/forms.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User name:')
-    email_address = StringField(label='Email Address:')
-    password1 = PasswordField(label='Password:')
-    password2 = PasswordField(label='Confirm Password:')
+    username = StringField(label='User name:', validators = [Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='Email Address:', validators= [Email(),DataRequired()])
+    password1 = PasswordField(label='Password:', validators = [Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:',validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
 
